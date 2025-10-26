8960fb9f549641c926200d75d120fdc382b89afd
Stas <stastokaruk1605@gmail.com>
2025-10-26 17:07:58 +0200

    test services

--- src/main/java/org/example/cosmocatsintergalacticmarketplace/service/CartServiceImpl.java
+++ src/main/java/org/example/cosmocatsintergalacticmarketplace/service/CartServiceImpl.java
@@ -15,7 +15,7 @@ public class CartServiceImpl implements CartService {
     private final AtomicLong idGen = new AtomicLong(0);
 
     @PostConstruct
-    private void initMockData() {
+    void initMockData() {
         store.put(1L, new Cart(1L, new ArrayList<>(), 0.0));
         store.put(2L, new Cart(2L, new ArrayList<>(), 0.0));
         idGen.set(2);
